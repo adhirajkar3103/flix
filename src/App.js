f3d2f5d721319071f90080ca23bbf1bfb4272c94
adhirajkar3103 <adhirajkar3103@gmail.com>
2023-06-29 14:29:28 +0530

    account functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import SavedShows from '../components/SavedShows'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+      <div className='w-full text-white'>
+      <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d282a426-b01a-424c-9b83-2c2445e4b61a/f7eb3bc2-2867-4c7e-94f8-e62ec11175cd/IN-en-20230626-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt=""
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
+          <div className='absolute top-[20%] p-4 md:p-8'></div>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
   )
 }
 
